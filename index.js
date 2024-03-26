285cc8bcda3623a05f4e027c4518f267b5c177ab
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-25 21:50:20 -0700

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+    let cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return cards;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
